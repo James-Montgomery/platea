ff78b7bd91a30aaf788ed9e59e5a653d6c7b30c0
James-Montgomery <jamesoneillmontgomery@gmail.com>
2020-02-09 18:00:58 -0500

    minor setup revision

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ import versioneer
 # python3 -m pip install --user --upgrade setuptools wheel
 # rm -rf build && rm -rf dist && rm -rf *.info
 # python3 setup.py sdist bdist_wheel
-# python3 -m twine upload --repository-url https://upload.pypi.org/legacy/ dist/*
+# python3 -m twine upload --verbose --repository-url https://upload.pypi.org/legacy/ dist/*
 
 ################################################################################
 
